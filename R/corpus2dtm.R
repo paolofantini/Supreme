bc03e7435db4b2a97ce65828b17849496096186f
paolofantini <paolofantini@hotmail.com>
2015-07-14 13:41:11 +0200

    Update corpus2dtm.R

--- R/corpus2dtm.R
+++ R/corpus2dtm.R
@@ -37,9 +37,9 @@ corpus2dtm <- function(corpus) {
   # List of tm transformations (bottom-up order).
   funs <- list(content_transformer(stripWhitespace),
                content_transformer(removeItalianStopWords),
-			         content_transformer(removeNumbers),
-			         content_transformer(removePunctuation),
-			         content_transformer(tolower))
+               content_transformer(removeNumbers),
+               content_transformer(removePunctuation),
+	       content_transformer(tolower))
   dtmCorpus <- tm_map(dtmCorpus, FUN = tm_reduce, tmFuns = funs)
 
   # dtm: keep only terms with minimum length 3 and appearing at least in 2 documents.
